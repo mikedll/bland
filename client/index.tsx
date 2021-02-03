39674c6506a447585e0056d20529e3c7c2130194
Michael Rivera <soymrmike@gmail.com>
2021-02-02 20:44:02 -0800

    Can render list of people from server.

--- client/index.tsx
+++ client/index.tsx
@@ -5,9 +5,71 @@ import React from 'react';
 
 import ReactDOM from 'react-dom'
 
+interface PersonProps {};
+
+interface Person {
+  id: number,
+  name: string
+}
+
+interface PersonState {
+  persons: Person[],
+  fetched: boolean,
+  error: string
+}
+
+class PersonList extends React.Component {
+  state: PersonState;
+  
+  constructor(props?: PersonProps) {
+    super(props);
+    this.state = {
+      persons: [],
+      fetched: false,
+      error: ""
+    }
+  }
+
+  componentDidMount() {
+    if(!this.state.fetched) {
+
+      fetch('/people')
+        .then(response => response.json())
+        .then(data => this.setState({fetched: true, persons: data}))
+        .catch(e => {
+          this.setState({fetched: true, error: "An error occurred while fetching records."});
+        })
+      
+    }
+  }
+  
+  render() {
+
+    const errorMsg = this.state.error !== '' ? (<div className={"alert alert-danger"}>{this.state.error}</div>) : null;
+    
+    const lis = this.state.persons.map((person) => (
+      <li key={person.id}>{person.name}</li>
+    ));
+    
+    return (
+      <div>
+        
+        {errorMsg}
+        
+        <ul className="people">{lis}</ul>
+        
+      </div>
+    );
+  }
+}
+
+
 const App = () => {
   return (
-    <div className="app">Welcome to the app.</div>
+    <div className="app">
+      <div className="app">Welcome to the app.</div>
+      <PersonList/>
+    </div>
   );
 }
 
